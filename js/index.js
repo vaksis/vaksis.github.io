826608a4691a4d3aa9aa60e505d2bb06bb244df0
vaksis YT <veetixx8@gmail.com>
2021-09-23 20:37:22 +0300

    added magic button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-//placeholder
+function taikanappi() {
+    var code = prompt('Insert the 6-digit code:');
+    if (code > 100 && code < 400000){
+        open("https://nhentai.net/g/" + code,);
+    } else {
+        location.reload(true);
+    }
+};
+function adbutton() {
+    open("./ads/" + (Math.floor(Math.random() * 32)) + ".webp");
+};
